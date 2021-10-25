f115aa52b4eced084a36e918ebd1361cc4a478e1
heinzrm <heinzr@hotmail.com>
2021-10-24 23:55:39 -0300

    Esconder Menu

--- Source/View/ContasAPagar.View.Principal.pas
+++ Source/View/ContasAPagar.View.Principal.pas
@@ -16,18 +16,26 @@ uses
   FMX.ListBox,
   FMX.Layouts,
   FMX.Controls.Presentation,
-  FMX.MultiView, ContasAPagar.View.Cartoes;
+  FMX.MultiView, ContasAPagar.View.Cartoes, FMX.Objects, FMX.StdCtrls;
 
 type
   TfrmPrincipal = class(TForm)
+    LayoutMain: TLayout;
     MultiView1: TMultiView;
     ListBox1: TListBox;
+    ltbDashBoard: TListBoxItem;
     ltbCartoes: TListBoxItem;
-    ListBoxItem2: TListBoxItem;
     ListBoxItem3: TListBoxItem;
     ListBoxItem4: TListBoxItem;
-    LayoutMain: TLayout;
+    lytBase: TLayout;
+    ListBoxItem1: TListBoxItem;
+    lytTopo: TLayout;
+    lytMestre: TLayout;
+    Rectangle1: TRectangle;
+    btnMasterbuttom: TButton;
     procedure ltbCartoesClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure ListBox1Change(Sender: TObject);
   private
     { Private declarations }
   public
@@ -43,6 +51,16 @@ uses
   ContasAPagar.Diversos.LoadLayout;
 
 {$R *.fmx}
+
+procedure TfrmPrincipal.FormCreate(Sender: TObject);
+begin
+  MultiView1.Mode := TMultiViewMode.Drawer;
+end;
+
+procedure TfrmPrincipal.ListBox1Change(Sender: TObject);
+begin
+  MultiView1.HideMaster;
+end;
 
 procedure TfrmPrincipal.ltbCartoesClick(Sender: TObject);
 begin
